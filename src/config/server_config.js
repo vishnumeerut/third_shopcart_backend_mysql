725659946826571202bd3eb14c4f409aa677ae61
Vishnu Kumar <vermaji1891@gmail.com>
2025-03-17 16:17:49 +0530

    my sql db connection with local mysql workbench

--- src/config/server_config.js
+++ src/config/server_config.js
@@ -4,5 +4,10 @@ dotenv.config();
 
 
 module.exports = {
-    PORT: process.env.PORT || 3030
+    PORT: process.env.PORT || 3030,
+    DB_NAME:process.env.DB_NAME,
+    DB_PASS:process.env.DB_PASS,
+    DB_USER:process.env.DB_USER,
+    DB_URL:process.env.DB_URL,
+
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ const express = require("express");
 var responseTime = require('response-time')
 
 const  app = express();
+const mysql = require("mysql2")
 const bodyParser = require("body-parser");
-const {PORT} = require("./config/server_config")
+const {PORT, DB_NAME, DB_PASS, DB_URL, DB_USER} = require("./config/server_config")
 const apiRouter = require("./routes/api_router");
 
+const db = mysql.createConnection({
+    host     : DB_URL,
+    user     : DB_USER,
+    password : DB_PASS,
+    database : DB_NAME
+  })
 
 
 app.use(responseTime())
@@ -19,4 +26,19 @@ app.use("/api", apiRouter);
 
 app.listen(PORT, (req, res) => {
     console.log(`App is listening on port no:-> ${PORT}`)
+    db.connect((err) => {
+        if(err) {
+            console.log("DB Not Connect..")
+            console.log("Error is:", err)
+            throw err;
+        }
+        console.log("Db connected Successfully..")
+    })
+    db.query("select * from actor", (err, result) => {
+        if(err) {
+            console.log("error is:-", err)
+            throw err;
+        }
+        console.log(result)
+    })
 })
