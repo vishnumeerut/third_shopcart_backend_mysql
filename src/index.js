a1158830a6c4b8f4aa912b8cfd82936b5f9f1d23
Vishnu Kumar <vermaji1891@gmail.com>
2025-03-18 10:43:28 +0530

    db connection with sequelize orm

--- src/index.js
+++ src/index.js
@@ -2,17 +2,11 @@ const express = require("express");
 var responseTime = require('response-time')
 
 const  app = express();
-const mysql = require("mysql2")
 const bodyParser = require("body-parser");
-const {PORT, DB_NAME, DB_PASS, DB_URL, DB_USER} = require("./config/server_config")
+const { PORT } = require("./config/server_config")
 const apiRouter = require("./routes/api_router");
+const db = require("./config/db_config")
 
-const db = mysql.createConnection({
-    host     : DB_URL,
-    user     : DB_USER,
-    password : DB_PASS,
-    database : DB_NAME
-  })
 
 
 app.use(responseTime())
@@ -24,21 +18,9 @@ app.use(bodyParser.urlencoded({extended:true})) // for validating the data incom
 
 app.use("/api", apiRouter);
 
-app.listen(PORT, (req, res) => {
+app.listen(PORT, async (req, res) => {
     console.log(`App is listening on port no:-> ${PORT}`)
-    db.connect((err) => {
-        if(err) {
-            console.log("DB Not Connect..")
-            console.log("Error is:", err)
-            throw err;
-        }
-        console.log("Db connected Successfully..")
-    })
-    db.query("select * from actor", (err, result) => {
-        if(err) {
-            console.log("error is:-", err)
-            throw err;
-        }
-        console.log(result)
-    })
+    await db.sync()
+    console.log("Db connected Successfully....")
+
 })
